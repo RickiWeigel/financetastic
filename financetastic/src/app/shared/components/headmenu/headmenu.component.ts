d9639b3d4b3aeec14be7963956fcdd99761480a9
RickiWeigel <ricki.weigel@gmx.de>
2024-12-11 14:57:05 +0100

    install materialdesign

---
+++ financetastic/src/app/shared/components/headmenu/headmenu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headmenu',
+  imports: [],
+  templateUrl: './headmenu.component.html',
+  styleUrl: './headmenu.component.scss'
+})
+export class HeadmenuComponent {
+
+}
