7c7af35e9de69b72b68a8f90a19d6f6c511d5ce1
RickiWeigel <ricki.weigel@gmx.de>
2024-12-11 23:55:57 +0100

    creat main-content components

--- financetastic/src/app/app.routes.ts
+++ financetastic/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { SummaryComponent } from './main-content/summary/summary.component';
+import { AccountsComponent } from './main-content/accounts/accounts.component';
+import { EntriesComponent } from './main-content/entries/entries.component';
+import { StatisticsComponent } from './main-content/statistics/statistics.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: SummaryComponent },
+    { path: 'accounts', component: AccountsComponent },
+    { path: 'entries', component: EntriesComponent },
+    { path: 'statistics', component: StatisticsComponent },
+    { path: '**', redirectTo: '' } // Fallback-Route
+
+];
