2e6d6959d3b1b108e5494b2a96bf5aded2c72a07
Ricki W. <ricki.weigel@gmx.de>
2024-12-14 18:54:08 +0100

    create date-picker-menu

--- financetastic/src/app/main-content/summary/summary.component.ts
+++ financetastic/src/app/main-content/summary/summary.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatChipsModule} from  '@angular/material/chips' ;
+import { DatepickerComponent } from "../../shared/components/datepicker/datepicker.component";
 
 
 @Component({
   selector: 'app-summary',
-  imports: [MatIconModule,MatChipsModule],
+  imports: [MatIconModule, MatChipsModule, DatepickerComponent],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
---
+++ financetastic/src/app/shared/components/datepicker/datepicker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-datepicker',
+  imports: [MatIconModule],
+  templateUrl: './datepicker.component.html',
+  styleUrl: './datepicker.component.scss'
+})
+export class DatepickerComponent {
+
+}
