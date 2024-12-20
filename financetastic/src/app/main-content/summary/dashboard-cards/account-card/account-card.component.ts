5fe5de4a480dd33d4d2e3437c79bb4b3d4bbeb1e
RickiWeigel <ricki.weigel@gmx.de>
2024-12-20 12:40:52 +0100

    feat: Added new feature to display account type in dashboard card

--- financetastic/src/app/main-content/summary/dashboard-cards/account-card/account-card.component.ts
+++ financetastic/src/app/main-content/summary/dashboard-cards/account-card/account-card.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-account-card',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './account-card.component.html',
-  styleUrl: './account-card.component.scss'
+  styleUrl: './account-card.component.scss',
 })
 export class AccountCardComponent {
+
+  @Input() accountName: string = '';
+  @Input() accountType: string = '';
+  @Input() balance: string = '';
 
 }
