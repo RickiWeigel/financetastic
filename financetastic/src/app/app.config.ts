7c1804b4f386ff552fa62c44fa0de0f4eb11119f
RickiWeigel <ricki.weigel@gmx.de>
2025-01-02 00:19:01 +0100

    added firebase app

--- financetastic/src/app/app.config.ts
+++ financetastic/src/app/app.config.ts
@@ -3,7 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { firebaseConfig } from '../firebaseConfig';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(()=> getFirestore())
+  ],
 };
