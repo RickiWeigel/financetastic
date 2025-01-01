4c23ba33b8f9e2dd8dda5a71ee93470ca184438e
RickiWeigel <ricki.weigel@gmx.de>
2025-01-01 17:23:42 +0100

    Update Angular to the latest version and install @angular/fire for Firebase integration

--- financetastic/src/main.ts
+++ financetastic/src/main.ts
@@ -2,9 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "financetastic-7571a", appId: "1:167353425360:web:1983bb46a0ca63f774add5", storageBucket: "financetastic-7571a.firebasestorage.app", apiKey: "AIzaSyAI1-fueNR6Qir81mh-ZfOzRNhY4w8PcE4", authDomain: "financetastic-7571a.firebaseapp.com", messagingSenderId: "167353425360", measurementId: "G-6H5VCPLNS9" })), provideAnalytics(() => getAnalytics()), ScreenTrackingService, provideFirestore(() => getFirestore())
   ]
 }).catch(err => console.error(err));
